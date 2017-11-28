10f5294e50a4f474a3b50274aed18f91acf045cc
Anuj Deshmukh <anuj.deshmukh7@gmai>
2017-11-27 18:35:54 -0800

    Initial Commit

---
+++ RDTKnockOff/View Controller/RDTMasterVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RDTKnockOff
+//
+//  Created by Anuj Deshmukh on 11/27/17.
+//  Copyright © 2017 Anuj Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class RDTMasterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
